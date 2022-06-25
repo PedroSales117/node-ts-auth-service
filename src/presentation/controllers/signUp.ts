0ab541db003913b0915674fbd90ac0bfcc4d03a0
Pedro Henrique Pereira Sales <sales_pedro@terceiros.sicredi.com.br>
2022-06-25 18:36:59 -0300

    feat: add createUserMethod and authenticate using jwt

--- src/presentation/controllers/signUp.ts
+++
@@ -1,6 +0,0 @@
-import express, { Response, Request } from 'express'
-// import mongoose from 'mongoose'
-
-express().get('/createUser', (request: Request, response: Response) => {
-  response.status(200).json({ data: 'User created' })
-})
